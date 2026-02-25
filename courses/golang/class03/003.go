5c783bbf0e467fd5e7a54f41b95f7821eb523cef
nickolss <nickolasmaiaraujo@gmail.com>
2026-02-25 12:46:53 -0300

    feat: add 3 class

---
+++ courses/golang/class03/003.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// text := "Test"
+	// text = "Test Update"
+
+	// fmt.Println(text)
+
+	
+	text := "Test"
+	number := 10
+	float := 3.14
+	boolean := true
+
+	fmt.Println(text)
+	fmt.Println(number)
+	fmt.Println(float)
+	fmt.Println(boolean)
+}
