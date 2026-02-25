1fc9cb52ded0c4f5d1216826f5a7d730f2a0ee5d
nickolss <nickolasmaiaraujo@gmail.com>
2026-02-25 12:42:17 -0300

    feat: add 2 class

---
+++ courses/golang/class02/002.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var meter float64
+	// meter = 10
+	// var message string
+	// message = "The value of meter is: "
+	
+	// fmt.Print(message)
+	// fmt.Println(meter)
+
+	
+	message := "The value of meter is: "
+	meter := 10.0
+	fmt.Print(message)
+	fmt.Println(meter)
+}
