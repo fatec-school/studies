ce61835d2ef4a133d216a9aed8b59580bd6f2fc4
nickolss <nickolasmaiaraujo@gmail.com>
2026-02-26 13:06:51 -0300

    feat: add 6 class

---
+++ courses/golang/class06/006.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number uint8 = 255
+	fmt.Println(number)	
+}
+
+/*
+
+int8 - 8-bit signed integer
+int16 - 16-bit signed integer
+int32 - 32-bit signed integer
+int64 - 64-bit signed integer
+
+uint8 - 8-bit unsigned integer
+uint16 - 16-bit unsigned integer
+uint32 - 32-bit unsigned integer
+uint64 - 64-bit unsigned integer
+
+float32 - 32-bit floating-point number
+float64 - 64-bit floating-point number
+
+int - platform-dependent signed integer (either 32 or 64 bits)
+uint - platform-dependent unsigned integer (either 32 or 64 bits)
+*/
