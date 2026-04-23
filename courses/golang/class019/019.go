50b75ad2b96ef97ad0aa3bb025d1be9b53ef76e1
nickolss <nickolasmaiaraujo@gmail.com>
2026-04-23 10:55:19 -0300

    feat: add class explain diference between slice and array

---
+++ courses/golang/class019/019.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var slideNumbers = []int{1, 2, 4, 54, 5} // Slide
+	var arrayNumbers = [2]int{0,2} // Array
+
+	fmt.Println(slideNumbers)
+	fmt.Println(arrayNumbers)
+}
