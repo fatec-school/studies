149cbf1a7fdd91a2e31492523c269e4e633c188b
nickolss <nickolasmaiaraujo@gmail.com>
2026-05-27 13:16:31 -0300

    feat: add testify lib

--- courses/golang/projects/email/internal/domain/campaign/campaign_test.go
+++ courses/golang/projects/email/internal/domain/campaign/campaign_test.go
@@ -2,22 +2,30 @@ package campaign
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewCampaign(t *testing.T) {
+	assert := assert.New(t)
 	name := "Campaign x"
 	content := "value"
-	contacts := []string{"email@gmail.com", "email2@gmail.com"}
+	contacts := []string{"email@email.com", "email2@email.com"}
 
 	campaign := NewCampaign(name, content, contacts)
 
-	if campaign.ID != "1" {
-		t.Errorf("id expected 1: %s", campaign.ID)
-	} else if campaign.Name != name {
-		t.Errorf("expected name %s: %s", name, campaign.Name)
-	} else if campaign.Content != content {
-		t.Errorf("expected content %s: %s", content, campaign.Content)
-	} else if len(campaign.Contacts) != len(contacts) {
-		t.Errorf("expected %d emails: %d", len(contacts), len(campaign.Content))
-	}
+	// if campaign.ID != "1" {
+	// 	t.Errorf("id expected 1: %s", campaign.ID)
+	// } else if campaign.Name != name {
+	// 	t.Errorf("expected name %s: %s", name, campaign.Name)
+	// } else if campaign.Content != content {
+	// 	t.Errorf("expected content %s: %s", content, campaign.Content)
+	// } else if len(campaign.Contacts) != len(contacts) {
+	// 	t.Errorf("expected %d emails: %d", len(contacts), len(campaign.Content))
+	// }
+
+	assert.Equal(campaign.ID, "1")
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
 }
