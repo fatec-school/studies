57e84509cb7cde4f8b10a89069aa9419edae69fb
nickolss <nickolasmaiaraujo@gmail.com>
2026-06-17 13:30:41 -0300

    feat: add post path class

--- courses/golang/projects/email/cmd/api/main.go
+++ courses/golang/projects/email/cmd/api/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
+	"email/internal/adapter/dto"
+	"email/internal/domain/campaign/service"
 	"net/http"
 
+	"github.com/gin-gonic/gin/render"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type product struct {
@@ -17,57 +18,22 @@ type product struct {
 func main() {
 	r := chi.NewRouter()
 
-	r.Use(myMiddleware)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		status := map[string]string{"status": "ok"}
-		w.Header().Set("Content-Type", "application/json")
-
-		resp, _ := json.Marshal(status)
-		w.Write(resp)
-	})
-
-	// Path param
-	r.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
-		param := chi.URLParam(r, "productName")
-
-		w.Write([]byte(param))
-	})
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		param := r.URL.Query().Get("name")
-		if param != "" {
-			w.Write([]byte(param))
-		} else {
-			w.Write([]byte("hello"))
-		}
-
-	})
-
-	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		var product product
-		request, err := io.ReadAll(r.Body)
+	service := service.Service{}
+	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
+		var request dto.NewCampaignRequest
+		render.DecodeJSON(r.Body, &request)
+		id, err := service.Create(&request)
 		if err != nil {
-			w.Write([]byte("erro ao ler corpo da request"))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		err = json.Unmarshal(request, &product)
-		if err != nil {
-			w.Write([]byte("erro ao deserializar json"))
-			return
-		}
-
-		w.Write([]byte(product.Name))
+		render.Status(r, http.StatusCreated)
 	})
 
 	http.ListenAndServe(":8080", r)
-}
-
-func myMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("before")
-		next.ServeHTTP(w, r)
-		fmt.Println("after")
-	})
 }
