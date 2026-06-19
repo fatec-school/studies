380c08adf3c617af6c3465c26b2f747004d8eb0e
nickolss <nickolasmaiaraujo@gmail.com>
2026-06-19 13:18:54 -0300

    feat: clean main.go

--- courses/golang/projects/email/cmd/api/main.go
+++ courses/golang/projects/email/cmd/api/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"email/internal/adapter/dto"
-	"email/internal/domain/campaign/service"
 	"net/http"
 
-	"github.com/gin-gonic/gin/render"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -21,19 +18,6 @@ func main() {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-
-	service := service.Service{}
-	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
-		var request dto.NewCampaignRequest
-		render.DecodeJSON(r.Body, &request)
-		id, err := service.Create(&request)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		render.Status(r, http.StatusCreated)
-	})
 
 	http.ListenAndServe(":8080", r)
 }
